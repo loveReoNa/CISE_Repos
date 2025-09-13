0ba16c93857672305735746478d9e0d43831c185
1633640816@qq.com <1633640816@qq.com>
2025-09-13 15:42:50 +0800

    first commit

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
+import React from 'react'; 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
